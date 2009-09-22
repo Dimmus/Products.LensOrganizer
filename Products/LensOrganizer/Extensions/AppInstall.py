d4b5bc575ee03d12f93ef320b3cdeb7ca92aaedc
Josh LaPlace <josh@cnx.rice.edu>
2009-09-22 23:31:20 +0000

    CONNEXIONSSRV-34: Refactor to use GenericSetup

--- Products/LensOrganizer/Extensions/AppInstall.py
+++
@@ -1,8 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-def install(self):
-    # Re-purpose hack from LensMaker to disable the State tab for LensOrganizer
-    pa = getToolByName(self, 'portal_actions') 
-    action = pa.getActionObject('object_tabs/content_status_history')
-    if action.condition.text.find('Lens Organizer') == -1:
-        action.edit(condition=action.condition.text + " and object.Type() != 'Lens Organizer'")
